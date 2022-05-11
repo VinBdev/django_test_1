fc4ed35878b223723e0e147558496f27d386863e
VinBdev <vbpod77@gmail.com>
2022-05-11 17:32:41 +0000

    added models and connected to django db with names

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://8000-vinbdev-djangotest1-5jbox3q8vc5.ws-eu44.gitpod.io']
